e3ce362a88d5d8663c107c3495afbb9ed963b75b
Chema-a <jose.davalos4980@alumnos.udg.mx>
2021-04-19 10:00:07 -0500

    Agregado de Boton

--- Species/ui.R
+++ Species/ui.R
@@ -6,14 +6,17 @@ pageWithSidebar(
     headerPanel('Modelo de Distribucion de Especies'),
     sidebarPanel(
         textInput("species", h3("Nombre cientifico de especie"), 
-                  value = "Canis latrans"),  
+                  value = ""),  
         numericInput("occ_limit", h2("Limite de Ocurrencia"),value = 20),
     ),
+
     mainPanel(
+        actionButton("do", "Generar modelo"),
         textOutput('selected_species'),
         plotOutput('plot1'),
         plotOutput('plot2'),
         plotOutput('plot3'),
         plotOutput('plot4')
+       
     )
 )
